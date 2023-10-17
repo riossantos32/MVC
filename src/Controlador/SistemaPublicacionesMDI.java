8a0cc3251be36198171638b988abded79c8338c7
riossantos32 <133970496+riossantos32@users.noreply.github.com>
2023-10-16 19:37:52 -0600

    Add files via upload

---
+++ src/Controlador/SistemaPublicacionesMDI.java
@@ -0,0 +1,170 @@
+package Controlador;
+import Vista.*;
+
+/** @author HeidyHueteLuna
+ */
+public class SistemaPublicacionesMDI extends javax.swing.JFrame {
+           
+    private JInternalFrameAutores Autores; //objeto formulario Autores
+
+    /***/
+    public SistemaPublicacionesMDI() {
+        initComponents();
+        setTitle("Publicaciones"); //Agrega titulo al MDI
+        Autores= new JInternalFrameAutores(); //
+        
+        //Agregar los JInternalFrameal desktopane del MDI
+        desktopPane.add(Autores);
+    }
+
+    /***/
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        desktopPane = new javax.swing.JDesktopPane();
+        menuBar = new javax.swing.JMenuBar();
+        fileMenu = new javax.swing.JMenu();
+        openMenuItem = new javax.swing.JMenuItem();
+        saveMenuItem = new javax.swing.JMenuItem();
+        saveAsMenuItem = new javax.swing.JMenuItem();
+        exitMenuItem = new javax.swing.JMenuItem();
+        editMenu = new javax.swing.JMenu();
+        VerAutoresMenuItem = new javax.swing.JMenuItem();
+        helpMenu = new javax.swing.JMenu();
+        contentMenuItem = new javax.swing.JMenuItem();
+        aboutMenuItem = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        fileMenu.setMnemonic('f');
+        fileMenu.setText("File");
+
+        openMenuItem.setMnemonic('o');
+        openMenuItem.setText("Open");
+        fileMenu.add(openMenuItem);
+
+        saveMenuItem.setMnemonic('s');
+        saveMenuItem.setText("Save");
+        fileMenu.add(saveMenuItem);
+
+        saveAsMenuItem.setMnemonic('a');
+        saveAsMenuItem.setText("Save As ...");
+        saveAsMenuItem.setDisplayedMnemonicIndex(5);
+        fileMenu.add(saveAsMenuItem);
+
+        exitMenuItem.setMnemonic('x');
+        exitMenuItem.setText("Exit");
+        exitMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exitMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(exitMenuItem);
+
+        menuBar.add(fileMenu);
+
+        editMenu.setMnemonic('e');
+        editMenu.setText("Autor");
+
+        VerAutoresMenuItem.setMnemonic('t');
+        VerAutoresMenuItem.setText("Ver Autores");
+        VerAutoresMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                VerAutoresMenuItemActionPerformed(evt);
+            }
+        });
+        editMenu.add(VerAutoresMenuItem);
+
+        menuBar.add(editMenu);
+
+        helpMenu.setMnemonic('h');
+        helpMenu.setText("Help");
+
+        contentMenuItem.setMnemonic('c');
+        contentMenuItem.setText("Contents");
+        helpMenu.add(contentMenuItem);
+
+        aboutMenuItem.setMnemonic('a');
+        aboutMenuItem.setText("About");
+        helpMenu.add(aboutMenuItem);
+
+        menuBar.add(helpMenu);
+
+        setJMenuBar(menuBar);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(desktopPane, javax.swing.GroupLayout.DEFAULT_SIZE, 657, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(desktopPane, javax.swing.GroupLayout.DEFAULT_SIZE, 467, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void exitMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitMenuItemActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_exitMenuItemActionPerformed
+
+    private void VerAutoresMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_VerAutoresMenuItemActionPerformed
+        // Llama al método obtenerDatos para mostrarlos en la tabla del formulario
+        Autores.obtenerDatos();
+        Autores.setVisible(true); //Muestra el formulario JInternalFrameAutores
+    }//GEN-LAST:event_VerAutoresMenuItemActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(SistemaPublicacionesMDI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(SistemaPublicacionesMDI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(SistemaPublicacionesMDI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(SistemaPublicacionesMDI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new SistemaPublicacionesMDI().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem VerAutoresMenuItem;
+    private javax.swing.JMenuItem aboutMenuItem;
+    private javax.swing.JMenuItem contentMenuItem;
+    private javax.swing.JDesktopPane desktopPane;
+    private javax.swing.JMenu editMenu;
+    private javax.swing.JMenuItem exitMenuItem;
+    private javax.swing.JMenu fileMenu;
+    private javax.swing.JMenu helpMenu;
+    private javax.swing.JMenuBar menuBar;
+    private javax.swing.JMenuItem openMenuItem;
+    private javax.swing.JMenuItem saveAsMenuItem;
+    private javax.swing.JMenuItem saveMenuItem;
+    // End of variables declaration//GEN-END:variables
+
+}
---
+++ src/Modelo/DAOAutor.java
@@ -0,0 +1,59 @@
+package Modelo;
+import java.util.*;
+
+/*** @author santos rios
+ */
+public class DAOAutor {
+    //Método para insertar datos en la BD
+    public Autor Insertar(String cedula, String nombres, String apellidos, 
+            String email, java.sql.Date fechaNac) {
+        String transaccion ="INSERT INTO Autor (cedula,nombres,apellidos,email,fechaNac) VALUES ('"
+                + nombres + "', '"
+                + apellidos + "', '"
+                + email + "', '"
+                + cedula + "', '"
+                + fechaNac + "')'";
+        //Llama al método Actualizar ubicado en DataBase.java
+        if (new DataBase().Actualizar(transaccion) > 0) {
+                return new Autor(cedula, nombres, apellidos, email, fechaNac);
+                }
+        return null;                
+        }
+        //Método para actualizar un registro en la BD
+    public int Actualizar(int id,String nombres,String apellidos,String email,
+            String cedula, java.sql.Date fechaNac){
+        String transaccion ="UPDATE Autor SET nombres='"
+                + nombres + "', apellidos='"
+                + apellidos + "', email= '"
+                + email + "',fechaNac= '"
+                + fechaNac + "',cedula='"
+                + cedula + "', WHERE id_autor="
+                + id ;       
+        
+        return new DataBase().Actualizar(transaccion);
+    }
+        //Método para seleccionar todos los registros de la tabla 
+    public List obtenerDatos() {
+        String transaccion = "SELECT * FROM Autor";
+        //Llama a metodo Listar de DataBase.java
+        List<Map> registros = new DataBase().Listar(transaccion);
+        List<Autor> autores = new ArrayList(); //Arreglo de autores
+        //Ciclo que recorre cada registro y los agrega al arreglo autores
+        for (Map registro : registros){
+            Autor aut = new Autor ((int) registro.get("id_autor"),
+            (String) registro.get("nombres"),
+            (String) registro.get("apellidos"),
+            (String) registro.get("email"), 
+            (String) registro.get("cedula"),  
+            (java.sql.Date) registro.get("fechaNac"));
+        autores.add(aut);    
+        }
+    return autores; //Retorna todos los autores ubicados en la tabla de BD
+    }
+        //Método para eliminar un registro de la tabla en la BD
+    public int Eliminar(int id) {
+        String transaccion = "DELATE FROM Autor WHERE id_autor='"+ id +"'";
+        
+        return new DataBase().Actualizar(transaccion);
+    }
+}
